fe24dadce48f1703167824801241b7bb02b46e5f
palakkk lodhi <me@example.com>
2025-11-21 18:35:37 +0530

    init commit

---
+++ Main.java
@@ -0,0 +1,277 @@
+//import java.util.Scanner;
+//
+//
+//public class Main {
+//    public static void Main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.print("enter number: ");
+//        int a = sc.nextInt();
+//
+//        System.out.print("enter double: ");
+//        Float c = sc.nextFloat();
+//
+//        System.out.println("enter full line");
+//        String b = sc.next();
+//    }
+//}
+
+
+    //  class Main {
+//    public static void main(String[] args)y {
+//
+//        short a=130;
+//        byte b=(byte)a;
+//        System.out.println(b);
+//
+//    }
+//}
+
+    //
+
+//
+
+
+//}
+//    public static void main(String[] args) {
+//        System.out.println("Hello World");
+//    }
+//}
+//   class Main {
+//    public static void main(String[] args) {
+//        String str="hello";
+//   /        System.out.println(str.toUpperCase());
+///        System.out.println(str.toLowerCase());
+///
+////        System.out.println(str.charAt(1));
+//        ;
+//
+//    }
+//}
+//  class Main {
+//      public static void main(String[] args) {
+//          String str = new String("hello");
+//          String str1 = new String ("hello") ;
+//
+//          System.out.println(str==(str1));
+//          System.out.println(str.equals(str1));
+//      }
+//}
+//  class Main {
+//      public static void main(String[] args) {
+//
+//          String str =" java ";
+//          int v=0;
+//          for(char ch : str.toCharArray()){
+//              if("".indexOf(ch)!=-1){
+//                  v++;
+//              }
+//          }
+///          String a[]=str.split(" ");
+////          for(String val : a){
+////              System.out.println(val);
+////          }
+//
+
+
+//
+
+    //
+//     class Bus{
+//    int ;
+//    String name;
+//    Bus(int a,String name){
+//        this.a=;
+//        this.name=name;
+//
+//
+//
+//
+//        System.out.println("name"+"+name");
+//        System.out.println("a"+ " "+a);
+//        class Main{
+//            public static void Main(String[] args){
+//             Bus s1=new Bus( 5,"jain") ;
+//             Bus s2=new Bus( 6,"");
+//             Bus s3=new Bus( 7,"mishra");
+//            }
+//        }
+//
+//
+//    }
+//}
+//  class Main {
+//    public static void main(String[] args) {
+//       Car s1=new Car(5);
+//
+//    }
+//}`
+//class car {
+//
+//    public car() {
+//        this(5);
+//        System.out.println("hello");
+//    }
+//
+//    public car(int a) {
+//        this(5,6);
+//        System.out.println(a);
+//
+//    }
+//
+//    public car(int a, int b) {
+//        System.out.println("a" + a + " b" + b);
+//    }
+//}
+//     public class Main{
+//        public static void main(String[] args) {
+//            car s1 = new car();
+//            car s2 = new car(5);
+//            car s3 = new car(5, 6);
+//        }
+//
+//    }
+//    class Animal {
+//    void eat() {
+//        System.out.println("bread");
+//    }
+//
+//            }
+//            class Dog extends Animal {
+//            void bark(){
+//                System.out.println("hehe");
+//            }
+//            }class Puppy extends Dog {
+//            void sleep(){
+//                System.out.println("KOI KAAM NHI");
+//            }
+//    }  class Main{
+//        public static void main(String[] args) {
+//            Puppy s1=new Puppy();
+//            s1.eat();
+//            s1.sleep();
+//            s1.bark();
+//        }
+//    }
+//  class parent {
+//    int x=10;
+//   parent(){
+//        System.out.println("Parent constructor:X="+x);
+//    }
+//}
+//   class Child extends parent {
+//    int x=20;
+//    Child() {
+//        System.out.println("child this.x="+this.x);
+//        System.out.println("Child super.x =" + super.x);
+//
+//    }
+//      void show() {
+//        int x =30;
+//        System.out.println("local.x =" + x);
+//          System.out.println("this.x =" + this.x);
+//          System.out.println("super.x =" + super.x);
+//      }
+//      public static void main (String[] args) {
+//        Child c =new Child();
+//        c.show();
+//      }
+//   }
+//
+//  class Shape{
+//    void Draw() {
+//        System.out.println("Draw");
+//
+//    }
+//    }
+//    class Circle extends Shape{
+//    void calculateArea(){
+//            System.out.println("area of circle= r  r");
+//        }
+//    }
+//    public class Main{
+//    public static void main (String[]args){
+//        Circle c =new Circle();
+//        c.Draw();
+//        c.calculateArea();
+//    }
+//    }
+//    class Book {
+//    void read(){
+//        System.out.println("All chemist");
+//    }
+//}
+//  class Novel extends Book{
+//    void storyline(){
+//          System.out.println("The novel has an intresting story line");
+//      }
+//  }
+//    public class Main{
+//    public static void main(String[]args){;
+//    Novel n=new Novel();
+//    n.read();
+//    n.storyline();
+//    }
+//    }
+//    class Animal{
+//    void eat(){
+//        System.out.println("pizza");
+//    }
+//}
+//  class Dog extends Animal{
+//    void eat(){
+//        System.out.println("roti");
+//    }
+//  }
+//    class {
+//    public static void main(String[] args) {
+//        Animal s1 = new Dog();
+//        s1.eat();
+//    }
+//    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
